135f4b7d439c2a095da8488cb62815f3d7d887bf
Arman Idrisi <119788224+Armanidrisi@users.noreply.github.com>
2023-10-03 18:03:40 +0530

    Update layout.js
    added the google tag

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+<head>
+<meta name="google-site-verification" content="HMgX-ATjh_v5C3Hgwq4Mn2zFncMh_qqeB8ZRXeOuTu0" />
+</head>
       <body className={poppins.className}>{children}</body>
     </html>
   );
